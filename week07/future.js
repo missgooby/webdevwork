912354c1b01d2d8f60c519a3ad669a681b21d29b
tahlil <89594239+missgooby@users.noreply.github.com>
2021-10-13 22:48:17 -0500

    Update future.js

--- week07/future.js
+++ week07/future.js
@@ -8,8 +8,9 @@ var numKids = [14,3,0,17,22,100,5,6,8];
 
 var xx = Math.floor(Math.random() * 4 );
 
-function fortune(job,city,spouse,numKids) {
-document.write("You will be a " + job + " in " + city + ", and married to " + spouce + "with" + numKids + " kids.")
+function fortune(job,city,spouse,numKids) 
+{
+  document.write("You will be a " + job + " in " + city + ", and married to " + spouce + "with" + numKids + " kids.");
 }
 
 fortune (job[xx],city[xx],spouse[xx],numKids[xx])
